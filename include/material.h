c09b42f7898a6c9e1c129fec9b7e932e448ad5fe
eavraam <e_avraam@outlook.com>
2024-04-09 12:54:51 +0200

    Simple dielectric material.

--- include/material.h
+++ include/material.h
@@ -15,6 +15,7 @@ public:
 	}
 };
 
+//> lambertian
 class Lambertian : public Material {
 public:
     Lambertian(const Color& albedo) : albedo(albedo) {}
@@ -35,7 +36,9 @@ public:
 private:
     Color albedo;
 };
+//< lambertian
 
+//> metal
 class Metal : public Material {
 public: 
     Metal(const Color& _albedo, double _fuzz)
@@ -53,5 +56,30 @@ private:
     Color albedo;
     double fuzz;
 };
+//< metal
 
+
+//> dielectric
+class Dielectric : public Material {
+public:
+    Dielectric(double _refraction_index) : refraction_index(_refraction_index) {}
+
+    bool scatter(const Ray& ray_in, const Hit_record& hit_rec, Color& attenuation, Ray& scattered) const override {
+        attenuation = Color(1.0, 1.0, 1.0);
+        double ri = hit_rec.front_face ? (1.0 / refraction_index) : refraction_index;
+
+        vec3 unit_direction(unit_vector(ray_in.direction()));
+        vec3 refracted = refract(unit_direction, hit_rec.normal, ri);
+
+        scattered = Ray(hit_rec.p, refracted);
+        return true;
+    }
+
+private:
+    double refraction_index;
+};
+
+
+
+//< dielectric
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main() {
 	// materials
 	auto material_ground = make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
 	auto material_center = make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
-	auto material_left   = make_shared<Metal>(Color(0.8, 0.8, 0.8), 0.3);
+	auto material_left   = make_shared<Dielectric>(1.5f);
 	auto material_right  = make_shared<Metal>(Color(0.8, 0.6, 0.2), 1.0);
 
 	// world
